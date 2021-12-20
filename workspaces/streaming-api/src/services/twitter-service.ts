257ff9c1a68c11e64e226d36c15d2935f473973f
Gabriel <grodriguez@emergya.com>
2021-12-20 22:20:51 +0100

    feat: subscribe to 'Javascript' o 'NodeJS' on Twitter with streams

--- workspaces/streaming-api/src/services/twitter-service.ts
+++ workspaces/streaming-api/src/services/twitter-service.ts
@@ -1,8 +1,37 @@
 import { logger } from "helpers"
+import { TwitterApi, ETwitterStreamEvent } from 'twitter-api-v2';
 
+async function setStreamRules(client){
 
-function init(config){
-  logger.info(config.token);
+  const tweetFields = {
+    'tweet.fields': ['referenced_tweets', 'author_id', 'created_at', 'public_metrics'],
+    'user.fields': ['name', 'username'],
+  };
+
+  await client.v2.updateStreamRules({
+    add: [{ value: 'JavaScript' }, { value: 'NodeJS' }],
+  });
+
+  return client.v2.searchStream(tweetFields);
+}
+
+function subscribeToStream(stream){
+  stream.autoReconnect = true;
+
+  stream.on(ETwitterStreamEvent.Data, async tweet => {
+    const isARt = tweet.data.referenced_tweets?.some(tweet => tweet.type === 'retweeted') ?? false;
+    if (isARt) {
+      return;
+    }
+    logger.info(JSON.stringify(tweet, null, 2));
+  });
+
+}
+
+async function init(config) {
+  const client = new TwitterApi(config.token);
+  const stream = await setStreamRules(client);
+  subscribeToStream(stream);
 }
 
 export default {
